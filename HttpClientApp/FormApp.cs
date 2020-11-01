85f380511474d6f3e6a28949fe132bc55f64151a
Dmitry Matin <super.dmitry320@yandex.ru>
2020-11-01 17:00:06 +0300

    Add project files.

---
+++ HttpClientApp/FormApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HttpClientApp
+{
+    public partial class FormApp : Form
+    {
+        public FormApp()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
